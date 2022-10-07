ab7dccba35432c0d0b1d1c51e8537e378263eace
felipevcc <felipevillamizarc@gmail.com>
2022-10-07 00:00:50 -0500

    file 4 c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - Prints only a text
  * Description: This function does to print the text
  * Return: 0
- */  
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
