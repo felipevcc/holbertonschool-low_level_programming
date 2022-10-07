7737faa91558a9668fc62cb336a14b8761980bee
felipevcc <felipevillamizarc@gmail.com>
2022-10-07 00:03:58 -0500

    file 5 c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints only a text
+ * Description: This function does to print the text
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
