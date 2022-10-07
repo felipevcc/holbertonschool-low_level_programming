c3986fdf15d8d6fc5d8fd757001996a2314d16c4
felipevcc <felipevillamizarc@gmail.com>
2022-10-07 10:33:03 -0500

    file 101-quote.c

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints only a text
+ * Description: This function does to print the text
+ * Return: 0
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
