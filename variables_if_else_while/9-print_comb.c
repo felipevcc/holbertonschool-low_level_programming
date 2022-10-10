bcd9699ffcbde54a133b578c324d85704053e3a9
felipevcc <felipevillamizarc@gmail.com>
2022-10-10 18:14:56 -0500

    file 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		if (num != 9)
+				printf(", ")
+	}
+
+	putchar('\n');
+
+	return (0);
+}
