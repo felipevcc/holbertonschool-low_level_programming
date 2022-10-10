144749ff4f0ee0f23045edf6530fcd1fe5659822
felipevcc <felipevillamizarc@gmail.com>
2022-10-10 17:39:54 -0500

    file 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	char letter; 
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
