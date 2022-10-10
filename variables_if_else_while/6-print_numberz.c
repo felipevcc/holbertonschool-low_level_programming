8d86e55d52c26ac7c872d313ac0f740fa3cba542
felipevcc <felipevillamizarc@gmail.com>
2022-10-10 18:03:25 -0500

    file 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
