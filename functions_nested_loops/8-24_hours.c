a1716b3ec89682e13728e6a64832f89f8d582150
felipevcc <felipevillamizarc@gmail.com>
2022-10-11 18:14:02 -0500

    file 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,16 +10,16 @@ void jack_bauer(void)
 	/* vars */
 	int h;
 	int m;
-	
+
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar(h / 10);
-			_putchar(h % 10);
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
 			_putchar(':');
-			_putchar(m / 10);
-			_putchar(m % 10);
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 			_putchar('\n');
 		}
 	}
