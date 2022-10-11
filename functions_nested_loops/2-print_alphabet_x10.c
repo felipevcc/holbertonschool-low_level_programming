dfba0320469f65d8dc385ceff6731f30d32a63d6
felipevcc <felipevillamizarc@gmail.com>
2022-10-11 12:23:55 -0500

    file 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints th alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int cont;
+
+	for (cont = 0; cont < 10; cont++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}	
+}
