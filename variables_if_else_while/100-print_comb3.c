cd7eac0f6dd20810e112a4a8326f34e965ac0ac3
felipevcc <felipevillamizarc@gmail.com>
2022-10-10 19:00:15 -0500

    file 100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -12,22 +12,23 @@ int main(void)
 	int num2;
 	int val = 1;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = '0'; num <= '8'; num++)
 	{
 		if (num == '8')
 			val = 0;
 
-		for (num2 = '1'; num <= '9'; num2++)
+		for (num2 = '1'; num2 <= '9'; num2++)
 		{		
 			if (num != num2 && num < num2)
 			{
 				putchar(num); 
 				putchar(num2);
-			}
-			if (val == 1)
-			{
-				putchar(',');
-				putchar(' ');
+
+				if (val == 1)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
