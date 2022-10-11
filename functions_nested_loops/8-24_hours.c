2379a5358ec8bbcab72e0f217eed37ddcaae3b74
felipevcc <felipevillamizarc@gmail.com>
2022-10-11 18:08:12 -0500

    file 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the time
+ *
+ */
+
+void jack_bauer(void)
+{
+	/* vars */
+	int h;
+	int m;
+	
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(h / 10);
+			_putchar(h % 10);
+			_putchar(':');
+			_putchar(m / 10);
+			_putchar(m % 10);
+			_putchar('\n');
+		}
+	}
+}
