007d35e73d00f90c01dd0d3d3c44ba4974edce9a
felipevcc <felipevillamizarc@gmail.com>
2022-10-11 12:44:43 -0500

    file 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - numbers conditionals
+ * @n: character check
+ *
+ * Return: 0 or 1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0); 
+	else
+		_putchar('-');
+		return (-1);
+}
