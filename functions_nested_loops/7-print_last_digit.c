25917da861293ff2afc0d2f4a068fcfa61425acd
felipevcc <felipevillamizarc@gmail.com>
2022-10-11 14:01:57 -0500

    file 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,11 @@
 
 int print_last_digit(int n)
 {
-	int last_digit;
-
 	if (n < 0)
 		n = n * -1;
 
-	last_digit = n % 10;
+	n = n % 10;
 
-	_putchar(last_digit + '0');
-	return (last_digit);
+	_putchar(n + '0');
+	return (n);
 }
