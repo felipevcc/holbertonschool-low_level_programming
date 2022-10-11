a40b141cb1c5071fd653ed48384a15a34d248540
felipevcc <felipevillamizarc@gmail.com>
2022-10-11 10:55:23 -0500

    file 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,13 +9,14 @@
 
 int main(void)
 {
-	char word[] = "_putchar\n";
+	char word[] = "_putchar";
 	int i;
 
 	for (i = '0'; i < strlen(word); i++)
 	{
 		putchar(word[i]);
 	}
+	putchar('\n')
 
 	return (0);
 }
