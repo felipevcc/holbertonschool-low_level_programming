099bf4305f5178008bed1b5790765d0024a8093c
felipevcc <felipevillamizarc@gmail.com>
2022-10-12 22:48:13 -0500

    file 100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -14,18 +14,15 @@ void print_times_table(int n)
 	int num;
 
 	num = 0;
-
 	for (line = 0; line <= n; line++)
 	{
 		if (n > 15 || n < 0)
 			break;
-
 		for (column = 0; column <= n; column++)
 		{
 			int num_result;
 
 			num_result = num * column;
-
 			if (num_result > 9 && num_result < 100)
 			{
 				_putchar(',');
