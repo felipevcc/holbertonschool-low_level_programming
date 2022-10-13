1fe0311a057d33c83f672ec82bb16cc398e31942
felipevcc <felipevillamizarc@gmail.com>
2022-10-12 21:54:07 -0500

    file 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+void print_to_98(int n)
+{
+	while (n == 98)
+	{
+		if (n > 98)
+		{
+			n--;
+		}
+		else
+		{
+			n++;
+		}
+		printf("%d, ", n);
+	}
+
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+}
