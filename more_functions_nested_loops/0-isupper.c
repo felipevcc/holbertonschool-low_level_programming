22aa8dd58ad1b25d826ea0a77db2461340163993
felipevcc <felipevillamizarc@gmail.com>
2022-10-13 16:48:58 -0500

    more functions

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - upper
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
