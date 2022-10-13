97ccd0d932813f852149cf24b06720e7b879d8dd
felipevcc <felipevillamizarc@gmail.com>
2022-10-12 21:58:49 -0500

    file 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,6 +12,8 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
+		printf("%d, ", n);
+		
 		if (n > 98)
 		{
 			n--;
@@ -19,12 +21,11 @@ void print_to_98(int n)
 		else
 		{
 			n++;
-		}
-		printf("%d, ", n);
+		}	
 	}
 
 	if (n == 98)
 	{
-		printf("%d", n);
+		printf("%d\n", n);
 	}
 }
