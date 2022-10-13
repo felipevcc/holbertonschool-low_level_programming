3d5b80f02d6a3885b61ae64e9836d9fc8fc35930
felipevcc <felipevillamizarc@gmail.com>
2022-10-12 21:32:25 -0500

    file 10 test

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -10,22 +10,10 @@
 int add(int n1, int n2)
 {
 	int add;
-	int counter;
-	int ex;
-	
+
 	add = n1 + n2;
-	counter = 0;
 
-	while (add > 0)
-	{
-		printf("add = %d", add);
-		add = add / 10;	
-		counter = counter + 1;
-	}
-	
-	ex = n1 + n2;
+	printf("%d", add);
 
-	printf("num = %d, digits = %d", ex, counter);
-	
 	return (0);
 }
