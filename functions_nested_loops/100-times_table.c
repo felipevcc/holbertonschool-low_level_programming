efe726da187e78970ae3980e135605baa766b789
felipevcc <felipevillamizarc@gmail.com>
2022-10-12 22:18:12 -0500

    file 100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -34,9 +34,9 @@ void print_times_table(int n)
 			else if (num_result > 99)
 			{
 				_putchar(',');
-				_putchar(' ');	
-				_putchar((num_result / 10) + '0');
+				_putchar(' ');
 				_putchar(((num_result / 10) / 10) + '0');
+				_putchar(((num_result / 10) % 10) + '0');
 				_putchar((num_result % 10) + '0');
 			}
 			else if (column == 0)
