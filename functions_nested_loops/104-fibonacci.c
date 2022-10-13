79858ca78602737d8a95a3342edb91c7bf97908e
felipevcc <felipevillamizarc@gmail.com>
2022-10-13 01:26:39 -0500

    file 104

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - the first 50 fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long n1 = 1;
+	long n2 = 2;
+	long last_n;
+	int i;
+
+	printf("%ld, %ld, ", n1, n2);
+
+	while (i < 96)
+	{
+		last_n = n1 + n2;
+		n1 = n2;
+		n2 = last_n;
+		if (i != 95)
+			printf("%ld, ", last_n);
+		else
+			printf("%ld\n", last_n);
+		i++;
+	}
+
+	return (0);
+}
