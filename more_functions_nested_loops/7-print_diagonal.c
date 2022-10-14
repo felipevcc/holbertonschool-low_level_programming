37ac808fb3f2448d0e12c2b6c704f0c41903d519
felipevcc <felipevillamizarc@gmail.com>
2022-10-13 22:39:04 -0500

    file 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints n number of times "\"
+ * @n: number of times
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
