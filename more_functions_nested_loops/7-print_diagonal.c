6d52d2a8ae9d71ecd9b55de3edbe68851be4d848
felipevcc <felipevillamizarc@gmail.com>
2022-10-13 22:48:38 -0500

    file 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,10 +8,17 @@
 void print_diagonal(int n)
 {
 	int i;
+	int i2;
 
 	for (i = 1; i <= n; i++)
 	{
+		for (i2 = 1; i2 <= i; i2++)
+		{
+			_putchar(' ');
+		}
 		_putchar(92);
+		_putchar('\n')
 	}
-	_putchar('\n');
+	if (n == 0)
+		_putchar('\n');
 }
