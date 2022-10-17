b98b644d3881aeae0f959f06ebea5700596fed70
felipevcc <felipevillamizarc@gmail.com>
2022-10-17 16:19:20 -0500

    file 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints some characters
+ * @str: string
+ *
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
