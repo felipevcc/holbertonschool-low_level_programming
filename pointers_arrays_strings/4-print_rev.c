f3178c60366e4ee29efc1ff328c56379bc9e1dc9
felipevcc <felipevillamizarc@gmail.com>
2022-10-17 11:25:15 -0500

    file 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int len = _strlen(s);
+
+	for (i = len; s[i] >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
