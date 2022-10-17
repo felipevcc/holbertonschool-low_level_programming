d06f14592408b03b3ef1858761db6a85bccf8aad
felipevcc <felipevillamizarc@gmail.com>
2022-10-17 11:47:47 -0500

    file 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -20,12 +20,13 @@ int _strlen(char *s)
 
 void print_rev(char *s)
 {
-	int i;
+	int i = _strlen(s);
 	/*int len = _strlen(s);*/
 
-	for (i = _strlen(s); s[i] > 0; i--)
+	while (s[i] != '\0')
 	{
 		_putchar(s[i]);
+		i--;
 	}
-	_putchar('\n');
+	_putchar('\n')
 }
