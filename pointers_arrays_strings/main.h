6a565f5e9f7ac365f5886c8b98dac4ca49ceab7d
felipevcc <felipevillamizarc@gmail.com>
2022-10-17 19:12:15 -0500

    More pointers, arrays and strings

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef HEADER_FIlE
 #define HEADER_FIlE 
 
+/* _putchar function */
 int _putchar(char c);
+
+/* Pointers, arrays and strings */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,5 +15,15 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
+/* More pointers, arrays and strings */
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
 
 #endif
