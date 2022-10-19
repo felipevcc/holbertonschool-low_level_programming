b80ad35646cd2ef1914871ba81e2bbe3a13e9f45
felipevcc <felipevillamizarc@gmail.com>
2022-10-19 11:23:35 -0500

    file 2.1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @src: string 1
+ * @dest: string 2
+ * @n: n bytes
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0, j = 0;
+	int i;
+
+	while (dest[len] != '\0')
+		len++;
+
+	for (i = len; src[j] != '\0'; i++, j++)
+	{
+		if (n >= j)
+			dest[i] = src[j];
+	}
+
+	return (dest);
+}
