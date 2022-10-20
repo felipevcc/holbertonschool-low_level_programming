19d8e7ac3e9c2244aae5903db6a379cbff10bc5e
felipevcc <felipevillamizarc@gmail.com>
2022-10-20 16:07:53 -0500

    file 2.6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,17 +9,24 @@
 char *cap_string(char *str)
 {
 	const int UPPERCASE = 32;
-	int i = 0, sep_index = 0;
+	int i = 0, sep_index = 0, upper = 0;
 	char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	while (str[i])
 	{
-		while (separators[sep_index])
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-				str[i] -= UPPERCASE;
-			else if (str[i] == separators[sep_index] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			str[i] -= UPPERCASE;
+			continue;
+		}
+
+		while (separators[sep_index])
+		{	
+			if (str[i] == separators[sep_index])
+				upper = 1;
+			else if (upper == 1 && str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] -= UPPERCASE;
+				upper = 0;
 			sep_index++;
 		}
 		i++;
