8507215c787b8b7a124de193c1d93d711f2c5737
felipevcc <felipevillamizarc@gmail.com>
2022-10-19 23:25:48 -0500

    file 2.3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer 1
+ * @s2: pointer 2
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		
+		s1++;
+		s2++;
+	}
+	return (0);
+}
