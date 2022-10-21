f109dda25d1b34db6ea515facd19907805b78881
felipevcc <felipevillamizarc@gmail.com>
2022-10-20 22:34:02 -0500

    file 3.2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: NULL or the first occurrence of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	return ('\0');
+}
