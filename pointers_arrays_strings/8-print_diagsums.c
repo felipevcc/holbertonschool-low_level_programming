ccb687442be5589bc32c3fd52ea9c00cdb444a1d
felipevcc <felipevillamizarc@gmail.com>
2022-10-21 18:51:57 -0500

    file 3.8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the diagonals of a square
+ * @a: array
+ * @size: array size
+ *
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	int lsum = 0, rsum = 0;
+
+	for (i = 0; i < size; i++)
+		lsum += a[i * (size + 1)];
+
+	for (j = 0; j < size; j++)
+	{
+		rsum += a[(j * size) + (size - 1 - j)];
+	}
+
+	printf("%ld, %ld\n", lsum, rsum);
+}
