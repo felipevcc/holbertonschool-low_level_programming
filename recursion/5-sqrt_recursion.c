eb16e17390384a723aa65e00338933cdc535eda5
felipevcc <felipevillamizarc@gmail.com>
2022-10-24 18:22:09 -0500

    file 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * root_recursion - returns the root of a number
+ * @n: number
+ * @i: aux
+ * Return: natural square root
+ */
+
+int root_recursion(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i > n)
+		return (-1);
+	else
+		root_recursion(n, i + 1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (root_recursion(n, 1));
+}
+
+int main(void)
+{
+	int r;
+
+	r = _sqrt_recursion(1);
+	printf("%d\n", r);
+	r = _sqrt_recursion(1024);
+	printf("%d\n", r);
+	r = _sqrt_recursion(16);
+	printf("%d\n", r);
+	r = _sqrt_recursion(17);
+	printf("%d\n", r);
+	r = _sqrt_recursion(25);
+	printf("%d\n", r);
+	r = _sqrt_recursion(-1);
+	printf("%d\n", r);
+	return (0);
+}
