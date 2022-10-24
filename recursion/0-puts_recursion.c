d676681e004e784abee727c5a1e0dc383e2b6cba
felipevcc <felipevillamizarc@gmail.com>
2022-10-24 09:20:15 -0500

    file 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	/* base case */
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		*s++;
+		_puts_recursion(s);
+	}
+}
+
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
