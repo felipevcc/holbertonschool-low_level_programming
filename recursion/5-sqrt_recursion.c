77c13d8d5890ce6bca0a6eff11bd823f9a628e4b
felipevcc <felipevillamizarc@gmail.com>
2022-10-24 18:26:35 -0500

    file 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * root_recursion - returns the root of a number
+ * _root_recursion - returns the root of a number
  * @n: number
  * @i: aux
  * Return: natural square root
  */
 
-int root_recursion(int n, int i)
+int _root_recursion(int n, int i)
 {
 	if (i * i == n)
 		return (i);
 	else if (i > n)
 		return (-1);
 	else
-	{
-		root_recursion(n, i + 1);
-	}
+		_root_recursion(n, i + 1);
 }
 
 /**
@@ -30,5 +28,5 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return (-1);
 	else
-		return (root_recursion(n, 1));
+		return (_root_recursion(n, 1));
 }
