b82188d90de2386ab7c80e594f461ad183d2f41d
felipevcc <felipevillamizarc@gmail.com>
2022-10-26 15:33:34 -0500

    file 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints args num, entry point
+ * @argc: args num
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argc);
+	(void)argv;
+	return (0);
+}
