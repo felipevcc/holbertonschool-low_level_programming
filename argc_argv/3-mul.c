cf96f965cc4b9d78215ed403969b367012b0626f
felipevcc <felipevillamizarc@gmail.com>
2022-10-26 16:04:22 -0500

    file 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,10 +11,10 @@
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	if (argv[1] && argv[2])
-	{	
+	{
 		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	else
-		printf("Error");
+		printf("Error\n");
 	return (0);
 }
