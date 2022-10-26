3fdc8429f2a287780b01962eedca01257b508f03
felipevcc <felipevillamizarc@gmail.com>
2022-10-26 16:40:23 -0500

    file 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -11,17 +11,20 @@
 int main(__attribute__((unused)) int argc, char *argv[])
 {
 	int counter = 0;
-	int i;
+	int i, j;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]))
-			counter += atoi(argv[i]);
-		else
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
+		if (atoi(argv[i]))
+			counter += atoi(argv[i]);	
 	}
 	printf("%i\n", counter);
 	return (0);
