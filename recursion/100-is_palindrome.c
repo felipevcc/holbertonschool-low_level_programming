825d7660657d21753d10c5256aaf3912761c3bf3
felipevcc <felipevillamizarc@gmail.com>
2022-10-28 12:52:03 -0500

    file 100

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * aux_palindrome - check palindrome
+ * @s: string
+ * @len: length str
+ * @i: iter var
+ * Return: 0 or 1
+ */
+
+int aux_palindrome(char *s, int len, int i)
+{
+	/* base case */
+	if (len == i)
+		return (1);
+	else if (s[i] != s[len - i])
+		return (0);
+	else
+		aux_palindrome(s, len, i + 1);
+}
+
+/**
+ * is_palindrome - return 0 or 1 if array is palindrome
+ * @s: string
+ * Return: 0 or 1
+ */
+
+int is_palindrome(char *s)
+{
+	char rev_str[] = {};
+	int len = 0, i = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (aux_palindrome(s, len - 1, i));
+}
