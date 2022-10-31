80424943404d158b031c87dfab3052fd640db424
felipevcc <felipevillamizarc@gmail.com>
2022-10-31 11:30:14 -0500

    file 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - returns a pointer, with a copy of a string
+ * @str: string
+ * Return: NULL or a pointer
+ */
+
+char *_strdup(char *str)
+{
+	int size = 0;
+	int i;
+	char *ar;
+
+	if (str == NULL)
+		return (NULL);
+	
+	while (str[size] != '\0')
+		size++;
+
+	ar = malloc(size);
+
+	for (i = 0; i < size; i++)
+	{
+		ar[i] = str[i];
+	}
+	return (ar);
+}
