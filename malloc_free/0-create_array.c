83da577208546f8fdf6c07ccffab16c2709abbfb
felipevcc <felipevillamizarc@gmail.com>
2022-10-31 10:11:53 -0500

    malloc_free dir

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *create_array - creates an array of chars and initializes it with a specific char
+ * @size: array size
+ * @c: char
+ * Return: NULL or a pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+	else if (c)
+		return (c);
+	return (NULL);
+}
