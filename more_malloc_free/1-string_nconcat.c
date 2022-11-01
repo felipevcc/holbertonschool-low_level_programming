6ff086a108bbdc27db3ab92062bd2cdd334af4e9
felipevcc <felipevillamizarc@gmail.com>
2022-11-01 16:56:47 -0500

    file 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * *string_nconcat - concatenates two strings
@@ -19,11 +20,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
+	/*
 	while (s1[len_s1] != '\0')
 		len_s1++;
 	while (s2[len_s2] != '\0')
 		len_s2++;
+	*/
+
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
 
 	ar = malloc(len_s1 + len_s2);
 	if (ar == NULL)
