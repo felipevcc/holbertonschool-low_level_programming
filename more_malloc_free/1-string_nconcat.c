ae3f4d9c61344172b01ed0799b242cbf4bc66d56
felipevcc <felipevillamizarc@gmail.com>
2022-11-01 17:04:01 -0500

    file 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[len_s2] != '\0')
 		len_s2++;
 
-	ar = malloc(len_s1 + len_s2);
+	ar = malloc(len_s1 + len_s2 + 1);
 	if (ar == NULL)
 		return (NULL);	
 
@@ -34,12 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ar[i] = s1[i];
 
 	if (n > len_s2)
-		n = len_s2;
+		n = len_s2 + 1;
 
-	for (j = 0; j < n; j++, i++)
+	for (j = 0; j <= n; j++, i++)
 		ar[i] = s2[j];
-	
-	ar[i] = '\0';
 
 	return (ar);
 }
