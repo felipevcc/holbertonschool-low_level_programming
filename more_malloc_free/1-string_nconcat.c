4096f35ee66cc81e53c2358112352365b45590dd
felipevcc <felipevillamizarc@gmail.com>
2022-11-01 16:54:33 -0500

    file 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: max size
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len_s1 = 0, len_s2 = 0;
+	unsigned int i, j;
+	char *ar;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len_s1] != '\0')
+		len_s1++;
+	while (s2[len_s2] != '\0')
+		len_s2++;
+
+	ar = malloc(len_s1 + len_s2);
+	if (ar == NULL)
+		return (NULL);	
+
+	for (i = 0; i < len_s1; i++)
+		ar[i] = s1[i];
+
+	if (n > len_s2)
+		n = len_s2;
+
+	for (j = 0; j <= n; j++, i++)
+		ar[i] = s2[j];
+	
+	return (ar);
+}
