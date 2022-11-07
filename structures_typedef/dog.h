614382162bd189271f9737bef43b8cb46b524c01
felipevcc <felipevillamizarc@gmail.com>
2022-11-07 10:46:10 -0500

    file dog 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ * dog - dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
