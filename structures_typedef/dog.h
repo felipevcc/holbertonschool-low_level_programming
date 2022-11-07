5784acbf415ed35a8905da0fd5da8e1e79149cc3
felipevcc <felipevillamizarc@gmail.com>
2022-11-07 17:42:43 -0500

    file header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -22,5 +22,7 @@ typedef struct dog dog_t;
 /* prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
