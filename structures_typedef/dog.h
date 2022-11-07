cb1cc0b460979f6194eb54772836b4c4412dace2
felipevcc <felipevillamizarc@gmail.com>
2022-11-07 16:54:58 -0500

    file dog 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,13 +10,14 @@
  * Description: basic information about a dog
  */
 
-typedef struct dog dog_t;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 /* prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
