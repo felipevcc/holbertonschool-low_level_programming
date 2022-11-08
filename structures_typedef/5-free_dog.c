0aa326a0c635a8ef33d1392b7e669f14d4aad468
felipevcc <felipevillamizarc@gmail.com>
2022-11-07 19:43:14 -0500

    file 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees a object
+ * @d: dog object
+ */
+
+void free_dog(dog_t *d)
+{
+	free(newDog->owner);
+	free(newDog->name);
+	free(newDog);
+}
