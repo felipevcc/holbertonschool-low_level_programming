7bacfb0919495e46697c8ebcfb0921494fe48534
felipevcc <felipevillamizarc@gmail.com>
2022-11-08 12:48:25 -0500

    file 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/* prototypes */
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
