16b9bcd749650189fd6dabefefb5138076a08359
felipevcc <felipevillamizarc@gmail.com>
2022-11-08 18:36:36 -0500

    file 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: numbers array
+ * @size: array legth
+ * @cmp: function, compare values
+ * Return: 0 or -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return (-1);
+}
