8c9d5528dc888c84aa5c3a3bca3ac11edee9dc75
felipevcc <felipevillamizarc@gmail.com>
2022-11-08 23:38:15 -0500

    files 3 calculator

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -19,8 +19,6 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL} };
 	int i = 0;
 
-	
-
 	while (ops[i].op)
 	{
 		if (*s == *ops[i].op && strlen(s) == 1)
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,14 +12,14 @@
 int main(int argc, char *argv[])
 {
 	int n1, n2;
-	int (*op_func)(int, int);	
+	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	
+
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
 	op_func = get_op_func(argv[2]);
