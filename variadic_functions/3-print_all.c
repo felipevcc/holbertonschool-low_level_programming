771e112e1ae1194f1656d2a718976bfbe4e0b2c5
felipevcc <felipevillamizarc@gmail.com>
2022-11-10 23:19:59 -0500

    file 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -25,10 +25,8 @@ void print_string(va_list list)
 
 	if (nil == NULL)
 		nil = "(nil)"; 
-	/*printf("%s", nil);*/
-	printf("hola");
+	printf("%s", nil);
 }
-
 
 /**
  * print_all - prints anything
@@ -36,35 +34,31 @@ void print_string(va_list list)
  */
 
 void print_all(const char * const format, ...)
-{
+{	
 	va_list args;
 	int i = 0;
 	int j;
-	char *separator = ", ";
+	char *separator = "";
 	type_st types[] = {
 		{"c", print_char},
 		{"i", print_int},
 		{"f", print_float},
 		{"s", print_string},
 		{NULL, NULL}
-	};
+	};	
 
 	va_start(args, format);
 
-	/*printf("Antes del while\n");*/
 	while (format && format[i])
-	{
-		/*printf("Entro a while 1\n");*/
+	{	
 		j = 0;
 		while (types[j].t)
 		{
-			/*printf("Entro a while 2\n");*/
-			/*printf("format = %c\n", format[i]);*/
-			/*printf("type = %s\n", types[j].t);*/
 			if (format[i] == *types[j].t)
 			{
 				printf("%s", separator);
-				types[i].f(args);	
+				types[j].f(args);
+				separator = ", ";
 			}
 			j++;
 		}
--- variadic_functions/tests/3-main.c
+++ variadic_functions/tests/3-main.c
@@ -1,4 +1,5 @@
 #include "../variadic_functions.h"
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-	print_all("ceis", 'B', 3, "stSchool");
+	print_all("fcsi", 3.14435, 'H', NULL, 402);
 	return (0);
 }
