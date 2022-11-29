c5b5d93e9130039900eef8eae4d10ad7e26e824a
felipevcc <felipevillamizarc@gmail.com>
2022-11-29 13:48:26 -0500

    file 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (!(*h).str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] (%s)\n", (*h).len, (*h).str);
+			printf("[%d] %s\n", (*h).len, (*h).str);
 		i++;
 		h = (*h).next;
 	}
