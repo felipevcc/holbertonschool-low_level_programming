9f807212661a9c4ff8d0db4fab566e051dc983c4
felipevcc <felipevillamizarc@gmail.com>
2022-11-29 13:46:51 -0500

    singly_linked_lists dir

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list
+ * @h: struct
+ * Return: int
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		if (!(*h).str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] (%s)\n", (*h).len, (*h).str);
+		i++;
+		h = (*h).next;
+	}
+	return (i);
+}
