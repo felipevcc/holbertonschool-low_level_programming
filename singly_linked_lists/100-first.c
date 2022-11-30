1e711b6c252a2c7fbfe7b638611f4880e900b827
felipevcc <felipevillamizarc@gmail.com>
2022-11-30 10:46:59 -0500

    file 100

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -6,6 +6,7 @@
 
 void __attribute__ ((constructor)) premain()
 {
-  printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
