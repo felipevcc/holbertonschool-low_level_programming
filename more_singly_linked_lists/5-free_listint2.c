6f81639411f745f035b5a23c981e6a1f42c710e0
felipevcc <felipevillamizarc@gmail.com>
2022-12-01 14:03:45 -0500

    file 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a structure
+ * @head: head of the structure
+ */
+
+void free_listint2(listint_t **head)
+{
+	if (!head)
+		return;
+
+	free_listint2(&head->next);
+	free(head);
+}
