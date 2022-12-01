97b9d1c5791b8d358c7665f0a0736bae91b1111c
felipevcc <felipevillamizarc@gmail.com>
2022-12-01 14:11:07 -0500

    file 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,16 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a structure
+ * free_listint2 - frees a structure and sets the head to NULL
  * @head: head of the structure
  */
 
 void free_listint2(listint_t **head)
 {
-	if (!(*head))
+	if (!head || !*head)
 		return;
 
 	free_listint2(&(*head)->next);
-	free(head);
+	free(*head);
+	*head = NULL;
 }
