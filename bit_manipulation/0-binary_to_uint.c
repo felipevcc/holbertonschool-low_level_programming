f7f7761d9e801c42fd59875fa910f2c6e700f4e8
felipevcc <felipevillamizarc@gmail.com>
2022-12-02 14:20:48 -0500

    bit_manipulation dir

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int bin_max_idx = strlen(b) - 1, multiplier = 1
+	unsigned int converted_int = 0;
+
+	if (!b)
+		return (0);
+
+	while (bin_max_idx >= 0)
+	{
+		if (b[bin_max_idx] != 0 && b[bin_max_idx] != 1)
+			return (0);
+
+		converted_int += multiplier * (b[bin_max_idx] - '0');
+		multiplier <<= 1;
+		bin_max_idx--;
+	}
+
+	return (converted_int);
+}
