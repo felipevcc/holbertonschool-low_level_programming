ca2a52a7ddf7cb63e993a50fe873ddd1c1131638
felipevcc <felipevillamizarc@gmail.com>
2022-12-02 10:12:29 -0500

    file 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * list_len - returns the number of elements in a linked list
+ * @h: struct
+ * Return: unsigned int
+ */
+
+unsigned int list_len(listint_t *h)
+{
+	unsigned int i = 0;
+
+	while (h)
+	{
+		i++;
+		h = (*h).next;
+	}
+	return (i);
+}
+
+/**
  * *insert_nodeint_at_index - inserts a new node at a given position
  * @head: head of the structure
  * @idx: index of the list where the new node should be added
@@ -13,19 +31,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *out, *new_node;
 	unsigned int i = 0;
 
-	if (!head || !*head)
+	if ((!*head && idx != 0) || idx > list_len(*head))
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
-	
+
 	out = *head;
 	new_node->n = n;
 	new_node->next = NULL;
 
+	if (idx == 0)
+	{
+		out = new_node;
+		return (new_node);
+	}
+
 	while (i < idx - 1)
-	{	
+	{
 		out = out->next;
 		i++;
 	}
--- more_singly_linked_lists/tests/9-main.c
+++ more_singly_linked_lists/tests/9-main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../lists.h"
 
 /**
