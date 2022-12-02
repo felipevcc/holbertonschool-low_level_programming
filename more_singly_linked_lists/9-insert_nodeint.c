ca59a7674661124b518cadcc039594b2cec70fab
felipevcc <felipevillamizarc@gmail.com>
2022-12-02 10:19:39 -0500

    file 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -44,7 +44,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		out = new_node;
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 
