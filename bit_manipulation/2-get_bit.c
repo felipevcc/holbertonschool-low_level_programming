f47f0054390ea72f5e81b9f0684c3f24c2a321d7
felipevcc <felipevillamizarc@gmail.com>
2022-12-02 23:47:09 -0500

    file 2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_bin - returns the value of a bit at a given index
+ * @n: number
+ * @index: index var
+ * Return: value of the bit at index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 63 || !n || !n >> 1)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
