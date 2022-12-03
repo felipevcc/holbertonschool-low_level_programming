3dff76f081a77a91d2004b25717754e77ef4ee84
felipevcc <felipevillamizarc@gmail.com>
2022-12-03 11:31:11 -0500

    file 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -2,12 +2,24 @@
 
 /**
  * flip_bits - returns num-bits you'd need to flip to get one number to another
- * @n: number
- * @m: var
- * Return: (int) '1' or '-1' if an error occurred
+ * @n: number 1
+ * @m: number 2
+ * Return: unsigned int
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return (1);
+	int i = 0;
+	unsigned int flips = 0;
+
+	while (i <= 63)
+	{
+		if ((n & 1) != (m & 1))
+			flips++;
+
+		n >>= 1;
+		m >>= 1;
+		i++;
+	}
+	return (flips);
 }
