407729ebc3c4d4f213f8d2f32fe4c4abb92b87cf
felipevcc <felipevillamizarc@gmail.com>
2022-12-03 00:09:16 -0500

    file 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  * Return: (int) '1' or '-1' if an error occurred
  */
 
-int clear_bit(unsigned long int *n, unsignedi int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns num-bits you'd need to flip to get one number to another
+ * @n: number
+ * @m: var
+ * Return: (int) '1' or '-1' if an error occurred
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (1);
+}
