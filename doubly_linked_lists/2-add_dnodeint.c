820d3fc2ec7de60c03fb48440b57d6628d503136
felipevcc <felipevillamizarc@gmail.com>
2022-12-06 11:21:07 -0500

    updates

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 
