761a7b63c0d2bd309b43175dfb41ab6ef33c0111
felipevcc <felipevillamizarc@gmail.com>
2022-12-07 15:39:05 -0500

    file 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,5 +9,30 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	return (letters);
+	int fd, vread, vwrite;
+	char *buff;
+
+	if (!filename || !letters)
+		return (0);
+
+	buff = malloc(letters);
+	if (!buff)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (!fd)
+		return (0);
+
+	vread = read(fd, buff, letters);
+	printf("%d", vread);
+	if (!vread)
+		return (0);
+
+	vwrite = write(STDOUT_FILENO, buff, letters);
+	if (!vwrite)
+		return (0);
+
+	close(fd);
+	free(buff);
+	return (vread);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Libraries */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 /* Prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
