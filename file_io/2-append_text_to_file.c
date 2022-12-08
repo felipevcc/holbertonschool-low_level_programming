23b647bd5a98462fe2872ee057bb8f38660973a1
felipevcc <felipevillamizarc@gmail.com>
2022-12-08 15:22:09 -0500

    file 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,13 +16,13 @@ int _strlen(char *s)
 }
 
 /**
- * create_file - creates a file
+ * append_text_to_file - appends text at the end of a file
  * @filename: name of the file to create
  * @text_content: string to write to the file
  * Return: 1 or -1 (int)
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	int content_len;
