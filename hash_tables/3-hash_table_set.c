c30c7ff7864bf25b8e385d8ac4b284ff87520119
felipevcc <felipevillamizarc@gmail.com>
2022-12-08 22:04:10 -0500

    file 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,5 +10,29 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *node = NULL;
+	char *value_dup = NULL, *key_dup = NULL;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	key_dup = strdup(key);
+	value_dup = strdup(value);
+
+	index = hash_djb2((const unsigned char *)key_dup) % (ht->size);
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+
+	node->key = key_dup;
+	node->value = value_dup;
+	node->next = NULL;
+
+	if (ht->array[index] && strcmp(ht->array[index]->key, key) != 0)
+		node->next = ht->array[index];
+
+	ht->array[index] = node;
+
 	return (1);
 }
