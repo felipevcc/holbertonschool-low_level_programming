1a0ae47b5867c5a829d993c6b9b312ec6cdee4f0
felipevcc <felipevillamizarc@gmail.com>
2022-12-09 03:17:23 -0500

    file 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -17,7 +17,7 @@ void hash_table_print(const hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		node = ht->array[i];
-		if (node)
+		while (node)
 		{
 			if (counter != 0)
 				printf(", ");
--- hash_tables/tests/5-main.c
+++ hash_tables/tests/5-main.c
@@ -12,15 +12,22 @@ int main(void)
 {
 	hash_table_t *ht;
 
-	ht = hash_table_create(1024);
-	hash_table_print(ht);
-	hash_table_set(ht, "c", "fun");
-	hash_table_set(ht, "python", "awesome");
-	hash_table_set(ht, "Bob", "and Kris love asm");
-	hash_table_set(ht, "N", "queens");
-	hash_table_set(ht, "Asterix", "Obelix");
-	hash_table_set(ht, "Betty", "Cool");
-	hash_table_set(ht, "98", "Battery Street");
+	ht = hash_table_create(30);
+	hash_table_set(ht, "USA", "Washington");
+	hash_table_set(ht, "France", "Paris");
+	hash_table_set(ht, "UK", "London");
+	hash_table_set(ht, "Spain", "Madrid");
+	hash_table_set(ht, "Germany", "Berlin");
+	hash_table_set(ht, "China", "Beijing");
+	hash_table_set(ht, "Russia", "Moscow");
+	hash_table_set(ht, "Bahrain", "Manama");
+	hash_table_set(ht, "Canada", "Ottawa");
+	hash_table_set(ht, "Brazil", "Brasilia");
+	hash_table_set(ht, "Sweden", "Stockholm");
+	hash_table_set(ht, "Iceland", "Reykjavik");
+	hash_table_set(ht, "Japan", "Tokyo");
+	hash_table_set(ht, "Australia", "Canberra");
+
 	hash_table_print(ht);
 	return (EXIT_SUCCESS);
 }
