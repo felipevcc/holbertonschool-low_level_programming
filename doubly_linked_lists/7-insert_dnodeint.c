99cba2eb373e3cf8783efc239c4572b35008991a
felipevcc <felipevillamizarc@gmail.com>
2022-12-09 00:12:33 -0500

    file 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,23 +1,4 @@
 #include "lists.h"
-#include <string.h>
-
-/**
- * list_len - returns the number of elements in a linked list
- * @h: struct
- * Return: unsigned int
- */
-
-unsigned int list_len(dlistint_t *h)
-{
-	unsigned int i = 0;
-
-	while (h)
-	{
-		i++;
-		h = (*h).next;
-	}
-	return (i);
-}
 
 /**
  * *insert_dnodeint_at_index - inserts a new node at a given position
@@ -34,30 +15,32 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!*h)
 		return (NULL);
-
-	if (idx > list_len(*h))
-		return (NULL);
+	
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	node_current = *h;
 
-	while (i < idx && node_current)
+	while (i < idx - 2)
 	{
+		if (!node_current)
+			return (NULL);
 		node_current = node_current->next;
 		i++;
 	}
+
+	if (!node_current->next)
+		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = node_current->prev;
-	new_node->next = node_current;
-
-	if (!node_current->prev)
-		*h = new_node;
-	else
-		node_current->prev->next = new_node;
+	new_node->next = node_current->next;	
+	new_node->prev = node_current;
+	node_current->prev->next = new_node;
+	node_current->next = new_node;
 
 	return (new_node);
 }
