b6fbb7ae66b5d3db1e04131fe4ec809733a76e88
felipevcc <felipevillamizarc@gmail.com>
2022-12-08 21:46:38 -0500

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -41,20 +41,18 @@ void error_cases(int error_status, char *file, int fd)
 int main(int argc, char **argv)
 {
 	int fd_from = 0, fd_to = 0, close_status = 0, i;
-	ssize_t bytes_read = 1;
+	ssize_t bytes_read = 1, bytes_written = 0;
 	char *file_from = NULL, *file_to = NULL;
 	char buff[1024];
 
 	if (argc != 3)
 		error_cases(97, NULL, 0);
-
 	file_from = argv[1];
 	file_to = argv[2];
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 		error_cases(98, file_from, 0);
-
 	fd_to = open(file_to, O_CREAT | O_RDWR | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -68,13 +66,16 @@ int main(int argc, char **argv)
 	while (bytes_read)
 	{
 		bytes_read = read(fd_from, buff, 1024);
-		write(fd_to, buff, bytes_read);
+		if (bytes_read == -1)
+			error_cases(98, file_from, 0);
+		bytes_written = write(fd_to, buff, bytes_read);
+		if (bytes_written == -1)
+			error_cases(99, file_to, 0);
 	}
 
 	close_status = close(fd_from);
 	if (close_status == -1)
 		error_cases(100, NULL, fd_from);
-
 	close_status = close(fd_to);
 	if (close_status == -1)
 		error_cases(100, NULL, fd_to);
