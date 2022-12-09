495488cba539bc84654533133bfc5c767a7ec1d8
felipevcc <felipevillamizarc@gmail.com>
2022-12-09 17:51:32 -0500

    file 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hast table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *node = NULL, *aux = NULL;
+
+	if (!ht)
+		return;
+
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			aux = node->next;
+			/*node = node->next;*/
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = aux;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
