9a1a6818c999a416839dc63b73665b2cdf0a7a78
felipevcc <felipevillamizarc@gmail.com>
2023-01-09 13:51:01 -0500

    tests/ update file 0

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef FILE_H
 #define FILE_H
 
+/* LIBRARIES */
+#include <stdio.h>
+
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
